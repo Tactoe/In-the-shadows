7b1106b3c6e456caa26da159364dfae20184a334
tactoe <tmilon@student.42.fr>
2020-03-19 19:35:06 +0100

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _gm;
+
+    public static GameManager Instance { get { return _gm; } }
+
+
+    private void Awake()
+    {
+        if (_gm != null && _gm != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _gm = this;
+            DontDestroyOnLoad(_gm);
+        }
+    }
+
+    public void ChangeScene(int sc)
+    {
+        if (SceneManager.sceneCountInBuildSettings > sc && sc >= 0)
+            SceneManager.LoadScene(sc);
+    }
+}
