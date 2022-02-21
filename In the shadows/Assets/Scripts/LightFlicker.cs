a3f88f4473ee98b5b917ea7ded2e0115c854fc6c
Tactoe <tmilon@student.42.fr>
2022-02-21 10:50:56 +0100

    Light baking should be ok

---
+++ In the shadows/Assets/Scripts/LightFlicker.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Light))]
+public class LightFlicker : MonoBehaviour
+{
+    [SerializeField]
+    private float m_MinVal;
+    [SerializeField]
+    private float m_MaxVal;
+    [SerializeField]
+    private float m_FlickerSpeed;
+    private bool m_Increasing;
+    private Light m_Light;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Light = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_Increasing)
+        {
+            m_Light.intensity += Time.deltaTime * m_FlickerSpeed;
+            if (m_Light.intensity > m_MaxVal)
+            {
+                m_Increasing = false;
+            }
+        }
+        else
+        {
+            m_Light.intensity -= Time.deltaTime * m_FlickerSpeed;
+            if (m_Light.intensity < m_MinVal)
+            {
+                m_Increasing = true;
+            }
+
+        }
+        
+    }
+}
